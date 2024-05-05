48ff4845ae2eaecf71a7b541c92d946c28535449
Josh D <joj237@gmail.com>
2024-05-05 11:54:53 -0500

    setup repo and react app for task 0

---
+++ react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import holbertonLogo from './holberton-logo.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={holbertonLogo} alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+      </body>
+      <footer className="App-footer">
+        <p>Copyright 2024 - holberton School</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
