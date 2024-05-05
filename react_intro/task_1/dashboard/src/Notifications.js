77a1a44e6257c4a7671768c2d569ca7a3f794a89
Josh D <joj237@gmail.com>
2024-05-05 14:05:40 -0500

    setup task 1 app with notifications

---
+++ react_intro/task_1/dashboard/src/Notifications.js
@@ -0,0 +1,12 @@
+// task_1/dashboard/src/Notifications.js
+
+import React from 'react';
+import './Notifications.css';
+
+export default function Notifications() {
+  return (
+    <div className="Notifications">
+      <p>Here is the of notifications</p>
+    </div>
+  );
+}
