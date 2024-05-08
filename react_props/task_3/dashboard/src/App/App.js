85f5ea8c632853cd11a1c310666cec122a141953
Josh D <joj237@gmail.com>
2024-05-07 19:50:54 -0500

    finished task 4 in task 3 folder but copying and leaving 3 as is since function reamains the same

--- react_props/task_3/dashboard/src/App/App.js
+++ react_props/task_3/dashboard/src/App/App.js
@@ -4,18 +4,28 @@ import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Login from '../Login/Login';
 import Notifications from '../Notifications/Notifications';
+import CourseList from '../CourseList/CourseList';
+import PropTypes from 'prop-types';
 
-function App() {
+function App({ isLoggedIn }) {
   return (
     <div className="App">
-      <Notifications />
+      <Notifications displayDrawer={isLoggedIn} />
       <Header />
-      <div className="App-body">
-        <Login />
+      <div className={`App-body ${isLoggedIn ? 'logged-in' : 'logged-out'}`}>
+        {isLoggedIn ? <CourseList /> : <Login />}
       </div>
       <Footer />
     </div>
   );
 }
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+};
+
+App.defaultProps = {
+  isLoggedIn: false,
+};
 
 export default App;
--- react_props/task_3/dashboard/src/Notifications/Notifications.js
+++ react_props/task_3/dashboard/src/Notifications/Notifications.js
@@ -2,8 +2,9 @@ import React from 'react';
 import './Notifications.css';
 import { getLatestNotification } from '../utils/utils';
 import NotificationItem from './NotificationItem';
+import PropTypes from 'prop-types';
 
-function Notifications() {
+function Notifications({ displayDrawer }) {
   const handleCloseClick = () => {
     console.log('Close button has been clicked');
   };
@@ -15,34 +16,47 @@ function Notifications() {
   ];
 
   return (
-    <div className="Notifications">
-      <button
-        style={{
-          position: 'absolute',
-          right: '10px',
-          top: '10px',
-          background: 'transparent',
-          border: 'none',
-          cursor: 'pointer',
-        }}
-        aria-label="Close"
-        onClick={handleCloseClick}
-      >
-        x
-      </button>
-      <p>Here is the list of notifications</p>
-      <ul>
-        {listNotifications.map((notification) => (
-          <NotificationItem
-            key={notification.id}
-            type={notification.type}
-            value={notification.value}
-            html={notification.html}
-          />
-        ))}
-      </ul>
-    </div>
+    <>
+      <div className="menuItem">Your notifications</div>
+      {displayDrawer && (
+        <div className="Notifications">
+          <button
+            style={{
+              position: 'absolute',
+              right: '10px',
+              top: '10px',
+              background: 'transparent',
+              border: 'none',
+              cursor: 'pointer',
+            }}
+            aria-label="Close"
+            onClick={handleCloseClick}
+          >
+            x
+          </button>
+          <p>Here is the list of notifications</p>
+          <ul>
+            {listNotifications.map((notification) => (
+              <NotificationItem
+                key={notification.id}
+                type={notification.type}
+                value={notification.value}
+                html={notification.html}
+              />
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
   );
 }
+
+Notifications.propTypes = {
+  displayDrawer: PropTypes.bool,
+};
+
+Notifications.defaultProps = {
+  displayDrawer: false,
+};
 
 export default Notifications;
