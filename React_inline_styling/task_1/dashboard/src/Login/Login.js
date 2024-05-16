45cfc5b4456c61777de8c897f456d1682a659f1b
Josh D <joj237@gmail.com>
2024-05-16 14:46:20 -0500

    change login to inline style

--- React_inline_styling/task_1/dashboard/src/Login/Login.js
+++ React_inline_styling/task_1/dashboard/src/Login/Login.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import './Login.css';
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+  loginForm: {
+    display: 'flex',
+    justifyContent: 'space-between', // Ensures space between elements
+    alignItems: 'center',
+    width: '100%',
+    marginBottom: '40px', // Add margin at the bottom of the form
+  },
+  label: {
+    marginRight: '10px', // Ensures space between label and input
+  },
+  input: {
+    flexGrow: 1,
+    marginRight: '10px', // Space between input and button
+  },
+});
 
 function Login() {
   return (
     <React.Fragment>
       <p>Login to access the full dashboard</p>
-      <form className="login-form">
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" />
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name="password" />
+      <form className={css(styles.loginForm)}>
+        <label htmlFor="email" className={css(styles.label)}>Email:</label>
+        <input type="email" id="email" name="email" className={css(styles.input)} />
+        <label htmlFor="password" className={css(styles.label)}>Password:</label>
+        <input type="password" id="password" name="password" className={css(styles.input)} />
         <button>OK</button>
       </form>
     </React.Fragment>
