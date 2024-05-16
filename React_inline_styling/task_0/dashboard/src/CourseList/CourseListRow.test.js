e40305af0051dbd862cae60e42f560dcf9563d57
Josh D <joj237@gmail.com>
2024-05-16 00:14:14 -0500

    Add inline styling to CourseListRow component

---
+++ React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import CourseListRow from './CourseListRow';
+
+describe('CourseListRow component tests', () => {
+  it('renders one cell with colspan = 2 when textSecondCell does not exist and isHeader is true', () => {
+    const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="Test" />);
+    expect(wrapper.find('th')).toHaveLength(1);
+    expect(wrapper.find('th').prop('colSpan')).toEqual(2);
+  });
+
+  it('renders two cells when textSecondCell is present and isHeader is true', () => {
+    const wrapper = shallow(
+      <CourseListRow isHeader={true} textFirstCell="Test" textSecondCell="Test2" />
+    );
+    expect(wrapper.find('th')).toHaveLength(2);
+  });
+
+  it('renders correctly two td elements within a tr element when isHeader is false', () => {
+    const wrapper = shallow(
+      <CourseListRow textFirstCell="Test" textSecondCell="Test2" />
+    );
+    expect(wrapper.find('tr')).toHaveLength(1);
+    expect(wrapper.find('tr').children('td')).toHaveLength(2);
+  });
+
+  it('applies the correct background color for header row', () => {
+    const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="Test" />);
+    expect(wrapper.prop('style')).toHaveProperty('backgroundColor', '#deb5b545');
+  });
+
+  it('applies the correct background color for non-header row', () => {
+    const wrapper = shallow(<CourseListRow textFirstCell="Test" />);
+    expect(wrapper.prop('style')).toHaveProperty('backgroundColor', '#f5f5f5ab');
+  });
+});
---
+++ React_inline_styling/task_0/dashboard/src/Notifications/NotificationItem.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NotificationItem from './NotificationItem';
+import Enzyme from 'enzyme';
+import Adapter from '@cfaester/enzyme-adapter-react-18';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('<NotificationItem />', () => {
+  it('renders without crashing', () => {
+    shallow(<NotificationItem type="default" />);
+  });
+
+  it('renders the correct HTML with type and value props', () => {
+    const wrapper = shallow(<NotificationItem type="default" value="test" />);
+    expect(wrapper.html()).toContain('<li data-notification-type="default">test</li>');
+  });
+
+  it('renders the correct HTML with html prop', () => {
+	const wrapper = shallow(<NotificationItem type="default" html={{ __html: '<u>test</u>' }} />);
+	expect(wrapper.html()).toContain('<li data-notification-type="default"><u>test</u></li>');
+  });
+
+  it('renders nothing if no value or html prop is provided', () => {
+    const wrapper = shallow(<NotificationItem type="default" />);
+    expect(wrapper.isEmptyRender()).toBe(true);
+  });
+  it('calls markAsRead with the right id when clicked', () => {
+    const markAsReadSpy = jest.fn();
+    const wrapper = shallow(
+      <NotificationItem id={1} type="default" value="test" markAsRead={markAsReadSpy} />
+    );
+
+    wrapper.simulate('click');
+
+    expect(markAsReadSpy).toHaveBeenCalledWith(1);
+  });
+});
