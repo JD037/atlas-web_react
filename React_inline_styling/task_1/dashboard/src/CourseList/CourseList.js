55590e95df178d300897a9b104b518e94a125f2a
Josh D <joj237@gmail.com>
2024-05-16 14:39:58 -0500

    add inline styles to courselist.js

--- React_inline_styling/task_1/dashboard/src/CourseList/CourseList.js
+++ React_inline_styling/task_1/dashboard/src/CourseList/CourseList.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
 import CourseListRow from './CourseListRow';
 import CourseShape from './CourseShape';
-import './CourseList.css';
 import WithLogging from '../HOC/WithLogging';
+
+const styles = StyleSheet.create({
+  courseList: {
+    width: '100%',
+    borderCollapse: 'collapse',
+    fontSize: '1.2rem',
+  },
+  th: {
+    border: '1px solid #ccc',
+    padding: '0.5rem',
+    textAlign: 'left',
+    backgroundColor: '#f2f2f2',
+    fontWeight: 'bold',
+  },
+  td: {
+    border: '1px solid #ccc',
+    padding: '0.5rem',
+    textAlign: 'left',
+  },
+});
 
 const CourseList = ({ listCourses }) => {
   return (
