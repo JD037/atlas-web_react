1e199cb50f520e971c4bcb8dc713bbc9504ce940
Josh D <joj237@gmail.com>
2024-05-27 12:24:32 -0500

    integrate Immutable.js with UI reducer and update test

---
+++ react_redux_reducer_selector/task_1/dashboard/src/actions/uiActionTypes.js
@@ -0,0 +1,8 @@
+// 0x08_react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionTypes.js
+
+export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
+export const DISPLAY_NOTIFICATION_DRAWER = 'DISPLAY_NOTIFICATION_DRAWER';
+export const HIDE_NOTIFICATION_DRAWER = 'HIDE_NOTIFICATION_DRAWER';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
