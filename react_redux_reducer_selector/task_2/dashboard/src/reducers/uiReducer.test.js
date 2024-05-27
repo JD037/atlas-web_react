a659b1b03a957b383d759d722b3ba8d5fccdea4d
Josh D <joj237@gmail.com>
2024-05-27 13:18:12 -0500

    add course reducer with load, select, and unselect actions and test

---
+++ react_redux_reducer_selector/task_2/dashboard/src/reducers/uiReducer.test.js
@@ -0,0 +1,35 @@
+// reducers/uiReducer.test.js
+
+import uiReducer from './uiReducer';
+import { Map } from 'immutable';
+import {
+  DISPLAY_NOTIFICATION_DRAWER,
+  HIDE_NOTIFICATION_DRAWER,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
+  LOGOUT
+} from '../actions/uiActionTypes';
+
+describe('uiReducer', () => {
+  const initialState = Map({
+    isNotificationDrawerVisible: false,
+    isUserLoggedIn: false,
+    user: {}
+  });
+
+  it('should return the initial state when no action is passed', () => {
+    expect(uiReducer(undefined, {})).toEqual(initialState);
+  });
+
+  it('should return the initial state when the action SELECT_COURSE is passed', () => {
+    const action = { type: 'SELECT_COURSE' };
+    expect(uiReducer(undefined, action)).toEqual(initialState);
+  });
+
+  it('should change isNotificationDrawerVisible to true when DISPLAY_NOTIFICATION_DRAWER is passed', () => {
+    const action = { type: DISPLAY_NOTIFICATION_DRAWER };
+    const expectedState = initialState.set('isNotificationDrawerVisible', true);
+    expect(uiReducer(undefined, action)).toEqual(expectedState);
+  });
+});
+
